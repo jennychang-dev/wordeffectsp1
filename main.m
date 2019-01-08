e2263b6051f0f58fe811d5c42051e7f34a1a56aa
Jenny Chang <chang.jc95@gmail.com>
2019-01-07 20:58:14 -0800

    updated again

--- main.m
+++ main.m
@@ -22,36 +22,55 @@ int main(int argc, const char * argv[]) {
        
         NSLog(@"input a string:");
         fgets(inputChars, 255, stdin); //places limit 255 characters
-            NSString *scannedChar = [NSString stringWithUTF8String:inputChars];
+        NSString *scannedChar = [NSString stringWithUTF8String:inputChars];
             
-        //convert char array to NSString object
-        
-        //print NSString object
-        //NSLog(@"Input was: %@", inputString);
-            
+            // if number is 1, then give case in upper case string
             if ([integer isEqual: @1]) {
                 NSString *uppercase = [scannedChar uppercaseString];
                 NSLog(@"uppercase of string is: %@\n",uppercase);
+            
+            // if number is 2, then give case in lower case string
+            } else if ([integer isEqual:@2]) {
+                NSString *lowercase = [scannedChar lowercaseString];
+                NSLog(@"lowercase of string is: %@\n",lowercase);
+            
+            // if number is 3, convert string into number
+            } else if ([integer isEqual:@3]) {
+                NSNumber *convert = @([scannedChar intValue]);
+                NSString *convertBack = [NSString stringWithFormat:@"%@",convert];
+              //  NSString *newConvertBack =
+                NSLog(@"convertback = %@", convertBack);
                 
-            } else
-            NSLog(@"go away!");
+                if([convertBack isNotEqualTo:convert]) {
+                    NSLog(@"can't successfully convert");
+                }
+                    
+                else { NSLog(@"number is %@\n", convert);
+            }
             
-//    NSString *uppercase = [inputString uppercaseString];
-//            NSLog(@"uppercase of string is: %@\n",uppercase);
-//
-//            NSString *lowercase = [inputString lowercaseString];
-//            NSLog(@"lowercase of string is: %@\n",lowercase);
-//
-//            char *scannedNumber;
-//            NSLog(@"input string to convert into number");
-//            fgets(scannedNumber,255,stdin);
-//            NSNumberFormatter *convert = [[NSNumberFormatter alloc] init];
-//            NSNumber *numbaaa = [convert numberFromString:scannedNumber];
-//            NSLog(@"new number is: %@\n",numbaaa);
+            // add ", eh?" to the end of a string
+            } else if ([integer isEqual:@4]) {
+                NSString *newString = [scannedChar substringToIndex:[scannedChar length]-1];
+                NSString *appendString = [newString stringByAppendingString:@",eh?"];
+                NSLog(@"the resulting string is %@\n",appendString); //why does it print ,eh line below?
+                
+            } else if ([integer isEqual:@5]) {
+                NSString *lastChar = [scannedChar substringToIndex:[scannedChar length]-1];
+                NSString *lastLastChar = [lastChar substringFromIndex:[lastChar length]-1];
+
+               if ([lastLastChar isEqual:@"?"]) {
+                  NSLog(@"I don't know");
+               }
+               else if ([lastLastChar isEqual:@"!"]) {
+                    NSLog(@"whoa, calm down!");
+                }
             
-            
-    }
+            // 
+            } else if ([integer isEqual:@6]) {
+                NSString *replaceSpace = [scannedChar stringByReplacingOccurrencesOfString:@" " withString:@"-"];
+                NSLog(@"new string is: %@",replaceSpace);
+            }
+        }
             return 0;
 }
 }
-
